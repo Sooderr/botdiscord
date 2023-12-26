713c516ab7ae9dc1b753a6030e916a3da6199012
root <root@DESKTOP-P5BQDEF>
2023-12-26 12:54:48 +0300

    ffd

--- src/message_listener.cpp
+++ src/message_listener.cpp
@@ -1,18 +1,20 @@
 #include "listeners/ message_listener.h"
-#include <selectaction/muteunmute/mute.cpp>
+#include "../include/selectaction/muteunmute/mute.cpp"
 
 void message_listener::select_click(const dpp::select_click_t& event) {
 dpp::cluster& bot = *event.from->creator;
-
     dpp::message message = event.command.msg;
         message.components = std::vector <dpp::component>(0);
-
         if (event.values[0] == "value1") {
             message.add_component(mute(bot, event));                    
+        }
+        else if(event.values[0] == "givemute")
+        {
+            event.dialog(givemutevoice(bot,event));
         }
          else if (event.values[0] == "exit") {
          bot.message_delete(event.command.msg.id, event.command.channel.id);
         }
-        event.edit_original_response(message);
-  
+        event.reply(dpp::ir_update_message,message);
+return;
 }
